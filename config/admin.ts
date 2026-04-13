a1751ca35af13acf9cb4a263ffe17ce7c6a2427e
creare-travel <direct@crearetravel.com>
2026-04-13 22:26:35 +0300

    admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -1,8 +1,16 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_AUTH_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+  secrets: {
+    encryptionKey: env('ENCRYPTION_KEY'),
   },
 });
