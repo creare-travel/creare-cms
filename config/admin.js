66c717ce01f6517bb9a131ee247f5fbc59104a84
creare-travel <direct@crearetravel.com>
2026-04-15 13:02:09 +0300

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,16 +1,16 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_AUTH_SECRET'),
+    secret: env('ADMIN_AUTH_SECRET') || 'myFallbackSecret123',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT') || 'myFallbackSalt123',
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT') || 'myFallbackTransfer123',
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY') || 'myFallbackEncryption123',
   },
 });
