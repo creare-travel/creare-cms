f3c84beafed7259901e6e6473eea2029f351a2db
creare-travel <direct@crearetravel.com>
2026-04-15 16:53:06 +0300

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -4,10 +4,15 @@ export default ({ env }: { env: any }): Core.Config.Database => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false,
+      host: env('DATABASE_HOST', 'postgres.railway.internal'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'railway'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false) && {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
       },
     },
+    debug: false,
   },
 });
